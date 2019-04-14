93d6a708869c2ebb77198864093da74c65c19410
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-04-14 15:44:32 -0700

    Add a toggle

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.scss';
-const Header = () => (
+
+const Header = () => {
+  const [collapsed, setCollapse] = useState(false);
+
+  return (
     <div className='Header'>
+      {collapsed ? <p>collapsed</p> : <p>not collapsed</p>}
+      <button onClick={() => setCollapse(!collapsed)}>Click This!</button>
       <h1>Header</h1>
     </div>
-);
+  );
+};
+
 export default Header;
