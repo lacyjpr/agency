59c1d88ef583973ffd9677c7b56c890fe0dbffdc
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-04-14 22:17:41 -0700

    Add scroll tracker to App.js

--- src/App.js
+++ src/App.js
@@ -11,9 +11,26 @@ import Contact from './Components/Contact';
 import Footer from './Components/Footer';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.x = 0;
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.scrollSpy);
+  }
+
+  scrollSpy = () => {
+    console.log('working');
+    console.log(window.scrollTop);
+
+    this.x++;
+    console.log(this.x);
+  };
+
   render() {
     return (
-      <div className='App'>
+      <div className='App' onScroll={this.scrollSpy()}>
         <Header />
         <Hero />
         <Services />
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.scss';
 
 const Header = () => {
   const [collapsed, setCollapse] = useState(false);
+
+  // useEffect(() => {
+  //   console.log(document.body.scrollTop);
+  // });
 
   return (
     <div className='Header'>
       {collapsed ? <p>collapsed</p> : <p>not collapsed</p>}
       <button onClick={() => setCollapse(!collapsed)}>Click This!</button>
       <h1>Header</h1>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 };
