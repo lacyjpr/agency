a9aefe0a52b9ad1db98c3e95f26b369eff1bc604
Jean-Paul Lacy <lacyjpr@gmail.com>
2019-05-30 14:27:55 -0700

    Add collapsing functionality

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
-//import ScrollspyNav from './ScrollspyNav';
 import Scrollspy from 'react-scrollspy';
 import './Header.scss';
 
 const Header = () => {
   let [yPosition, setYposition] = useState(null);
-  //const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
     window.addEventListener('scroll', scrollPosition);
@@ -31,84 +29,53 @@ const Header = () => {
     <div>
       <div className='Header'>
         <h1>Header</h1>
-        <Scrollspy
-          items={[
-            'services',
-            'portfolio',
-            'about',
-            'team',
-            'users',
-            'contact',
-            'footer',
-          ]}
-          currentClassName='nav__item--active'
-          className='nav__inner'
-          style={{
-            fontWeight: 300,
-          }}
-          offset={-440}
-          onUpdate={el => {
-            console.log(el);
-          }}
-        >
-          {/* <li className='nav__item'>
-            <a href='#hero' className='nav__link'>
-              Hero
-            </a>
-          </li>
-          <li className='nav__item'>
-            <a href='#services' className='nav__link'>
-              Services
-            </a>
-          </li>
-          <li className='nav__item'>
-            <a href='#portfolio' className='nav__link'>
-              Portfolio
-            </a>
-          </li> */}
-          <li className='Header__nav--link'>
-            <a href='#hero'> </a>
-          </li>
-          <li className='Header__nav--link'>
-            <a href='#services'>Services</a>
-          </li>
-          <li className='Header__nav--link'>
-            <a href='#portfolio'>Portfolio</a>
-          </li>
-          <li className='Header__nav--link'>
-            <a href='#about'>About</a>
-          </li>
-          <li className='Header__nav--link'>
-            <a href='#team'>Team</a>
-          </li>
-          <li className='Header__nav--link'>
-            <a href='#users'>Users</a>
-          </li>
-          <li className='Header__nav--link'>
-            <a href='#contact'>Contact</a>
-          </li>
-        </Scrollspy>
-
         <div className='Header__menu' onClick={navToggle}>
           ☰
         </div>
-        <ul className='Header__nav'>
-          <li className='Header__nav--link'>
-            <a href='#services'>Services</a>
-          </li>
-          <li className='Header__nav--link'>
-            <a href='#portfolio'>Portfolio</a>
-          </li>
-          <li className='Header__nav--link'>
-            <a href='#about'>About</a>
-          </li>
-          <li className='Header__nav--link'>
-            <a href='#team'>Team</a>
-          </li>
-          <li className='Header__nav--link'>
-            <a href='#contact'>Contact</a>
-          </li>
-        </ul>
+        <div className='Header__nav'>
+          <Scrollspy
+            items={[
+              'services',
+              'portfolio',
+              'about',
+              'team',
+              'users',
+              'contact',
+              'footer',
+            ]}
+            currentClassName='nav__item--active'
+            className='nav__inner'
+            style={{
+              fontWeight: 300,
+            }}
+            offset={-440}
+            onUpdate={el => {
+              console.log(el);
+            }}
+          >
+            <li className='Header__nav--link'>
+              <a href='#hero'> </a>
+            </li>
+            <li className='Header__nav--link'>
+              <a href='#services'>Services</a>
+            </li>
+            <li className='Header__nav--link'>
+              <a href='#portfolio'>Portfolio</a>
+            </li>
+            <li className='Header__nav--link'>
+              <a href='#about'>About</a>
+            </li>
+            <li className='Header__nav--link'>
+              <a href='#team'>Team</a>
+            </li>
+            <li className='Header__nav--link'>
+              <a href='#users'>Users</a>
+            </li>
+            <li className='Header__nav--link'>
+              <a href='#contact'>Contact</a>
+            </li>
+          </Scrollspy>
+        </div>
       </div>
     </div>
   );
